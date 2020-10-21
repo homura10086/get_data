c02a14bdec060c0e0a25c4d2dcdb5d9e28e7a6dc
彦伯 周 <1771931743@qq.com>
2020-10-21 21:16:37 +0800

    capacity

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from get_ran import *
-# 容量类问题
+# capacity problem
 # mode0 正常
-# mode1 覆盖质量类
+# mode1 覆盖质量类(越区/重叠覆盖）
 # mode2	切换类
 # mode3	基础资源类
 
 n = int(1e4)  # num_sample
 with open("data.csv", "w", newline="") as datacsv:
 	for i in range(n):
-		# mode = (i/n)*4
+		# mode = (i/n)*4  # for test
 		mode = randint(0, 3)
 		Get_Data(i, int(mode))
 		# filename1 = "Config_" + str(modes[i])
